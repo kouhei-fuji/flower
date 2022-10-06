989ded04f275749d745a838bf0808593741b63e0
exityan <exityan@gmail.com>
2022-10-06 20:20:28 +0900

    fix(examples): fix conversion between ndarray and bytes

--- src/py/flwr/common/parameter.py
+++ src/py/flwr/common/parameter.py
@@ -36,19 +36,10 @@ def parameters_to_ndarrays(parameters: Parameters) -> NDArrays:
 
 def ndarray_to_bytes(ndarray: NDArray) -> bytes:
     """Serialize NumPy ndarray to bytes."""
-    bytes_io = BytesIO()
-    # WARNING: NEVER set allow_pickle to true.
-    # Reason: loading pickled data can execute arbitrary code
-    # Source: https://numpy.org/doc/stable/reference/generated/numpy.save.html
-    np.save(bytes_io, ndarray, allow_pickle=False)  # type: ignore
-    return bytes_io.getvalue()
+    ndarray = ndarray.astype(np.float32) # TODO: Is this OK?
+    return ndarray.tobytes()
 
 
 def bytes_to_ndarray(tensor: bytes) -> NDArray:
     """Deserialize NumPy ndarray from bytes."""
-    bytes_io = BytesIO(tensor)
-    # WARNING: NEVER set allow_pickle to true.
-    # Reason: loading pickled data can execute arbitrary code
-    # Source: https://numpy.org/doc/stable/reference/generated/numpy.load.html
-    ndarray_deserialized = np.load(bytes_io, allow_pickle=False)  # type: ignore
-    return cast(NDArray, ndarray_deserialized)
+    return np.frombuffer(tensor, dtype=np.float32)
